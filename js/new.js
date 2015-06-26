d5399108d5fa75f6e36d4117ca41478362db9304
dragonman123 <pokemon7383@gmail.com>
2015-06-26 15:37:26 +0530

    completed, sent for review

--- js/new.js
+++ js/new.js
@@ -1,6 +1,9 @@
 // These are the test values
 // var A=[3,2.4,1.8,1.2,0.6,0] // A is the line with max x intercept
 // var B=[4,2,0]; // else B
+var flag=0;
+var arrayPoint=[];
+
 $(document).ready(function(){
 	$("#compositeFunction").hide();
 })
@@ -9,16 +12,10 @@ var miny=0, maxy=0;
 var yString;
 var flag1=0;
 var flag2=0;
-
-$(function () {
-	// inputHolderWidth();
-});
-
 var A=[] //A is the line with the max x intercept
 var B=[] //else B
 
 function drawChart(){
-	// alert(yString);
 	var chartData = {
 		"graphset":[
 		{
@@ -67,14 +64,14 @@ function fillArray(array,a,b,c){
 			maxy=y;
 		}
 	}
-	if(flag1==1 && flag2==1){
+	// if(flag1==1 && flag2==1){
 		maxy = parseInt(maxy) + 1;
 		if(maxy > 10){
 			maxy=10;
 		}
 		yString = miny.toString() + ":" + maxy.toString() + ":" + "1";
 		drawChart();
-	}
+	// }
 
 }
 var m,n;
@@ -91,39 +88,52 @@ function getValue(a,b,c){
 
 }
 
-function myFunc(a,b,c){
-	flag1=1;
-	var newObject = getValue(a,b,c);
-	a1=newObject.f;
-	b1=newObject.s;
-	c1=newObject.t;
-	fillArray(A,a1,b1,c1);
-	a=4;
-	b=5;
-	c=6;
-	newObject = getValue(a,b,c);
-	flag2=1;
-	a2=newObject.f;
-	b2=newObject.s;
-	c2=newObject.t;
-	fillArray(B,a2,b2,c2);
+function myFunc(a,b,c,decision){
+	if(decision==0){
+		flag1=1;
+		var newObject = getValue(a,b,c);
+		a1=newObject.f;
+		b1=newObject.s;
+		c1=newObject.t;
+		fillArray(A,a1,b1,c1);
+		$("#inputList1").fadeOut(1000, function() { $("#inputList1").remove(); });
+	}
+	else{
+		newObject = getValue(a,b,c);
+		flag2=1;
+		a2=newObject.f;
+		b2=newObject.s;
+		c2=newObject.t;
+		fillArray(B,a2,b2,c2);
+		$("#inputList2").fadeOut(1000, function() { $("#inputList2").remove(); });
 
-	console.log(a1,b1,c1,a2,b2,c2);
-	$(".inputValues").remove();
-	$("#compositeFunction").show();
-	// $("#compositeFunction").append('<button id="button2" class="buttons" onclick="location.reload(true)">Reset</button>');
-
-	$("#button3").click(function(){
-		// alert("Hello")
-		m=parseInt($("#input7").val());
-		n=parseInt($("#input8").val());
-		console.log(a1,b1,c1,a2,b2,c2);
-		calculateMax(a1,b1,c1,a2,b2,c2);
-	});
+	}
+	if(flag1==1 && flag2==1){
+		$("#compositeFunction").fadeIn(1000, function() { 
+			$(".inputValues").remove();
+			$("#compositeFunction").show();
+		});
+		setTimeout(function() {
+			console.log(a1,b1,c1,a2,b2,c2);
+			$("#button3").click(function(){
+				m=parseInt($("#input7").val());
+				n=parseInt($("#input8").val());
+				console.log(a1,b1,c1,a2,b2,c2);
+				calculateMax(a1,b1,c1,a2,b2,c2);
+			});
+		}, 1000);
+	}
 }
 
 
-var arrayPoint=[];
+function calcMin (a,b,m){
+	a = Math.min(a,b);
+	return {
+		val: m*a,
+		intercept: a
+	}
+}
+
 function calculateMax(a1,b1,c1,a2,b2,c2){
 	if( (a1*b2) == (a2*b1)){
 		alert("Inconsistent Equations");
@@ -131,57 +141,48 @@ function calculateMax(a1,b1,c1,a2,b2,c2){
 	}
 	x=((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1)); 
 	y=((c2*a1)-(c1*a2))/((a1*b2)-(a2*b1));
-	// c=parseInt($('#input7').val());
-	// d=parseInt($('#input8').val());
-	if(x>=0 && y>=0)
-	{ 
-		arrayPoint[3]=m*x+n*y;
-	//	alert("HEELOI i"); 
-}
-else
-	arrayPoint[3]=0;
-arrayPoint[0]=0;
-if(c1/a1 > c2/a2)
-	{
-		arrayPoint[1]=m*(c2/a2);
-		xIntercept=c2/a2;
-	}
-else
+	if(x>=0 && y>=0) arrayPoint[3]=m*x+n*y;
+	else arrayPoint[3]=0;
 
-	{
-		arrayPoint[1]=m*(c1/a1);
-		xIntercept=c1/a1;
-	}
-console.log(c1/a1);
-console.log(c2/a2);
-console.log("X and y are " + x + " and " + y);
-if(c1/b1 > c2/b2)
-	{
-		arrayPoint[2]=n*(c2/b2);
-		yIntercept=c2/b2;
-	}
-else
+	arrayPoint[0]=0;
+	
+	//Calculate for the x intercept
+	var tempDecision = calcMin(c1/a1, c2/a2, m);
+	arrayPoint[1] = tempDecision.val;
+	xIntercept = tempDecision.intercept;
 
-	{
-		arrayPoint[2]=n*(c1/b1);
-		yIntercept=c1/b1;
+	//Calculate for the y intercept
+	tempDecision = calcMin(c1/b1, c2/b2, n);
+	arrayPoint[2] = tempDecision.val;
+	yIntercept = tempDecision.intercept;
+
+	var timeDuration =1000;
+	var delay=2000;
+
+	setTimeout(function(){
+		$("#paragraph1").html("The value of the function at (" + xIntercept.toFixed(2) + ',' + '0' + ') is '   + arrayPoint[1].toFixed(2));
+		$("#paragraph1").animate({width:'toggle'},1000);
+	}, timeDuration);
+	timeDuration+=delay;
+
+	setTimeout(function(){
+		$("#paragraph2").html("The value of the function at (" + '0' + ',' + yIntercept.toFixed(2) + ') is '   + arrayPoint[2].toFixed(2));
+		$("#paragraph2").animate({width:'toggle'},1000);
+	}, timeDuration);
+
+	timeDuration+=delay;
+
+	if(x >=0 && y>=0){
+		setTimeout(function(){
+			$("#paragraph3").html("The value of the function at (" + x.toFixed(2) + ',' + y.toFixed(2) + ') is '   + arrayPoint[3].toFixed(2));
+			$("#paragraph3").animate({width:'toggle'},1000);
+		}, timeDuration);
 	}
-console.log(c1/b1);
-console.log(c2/b2);
-// console.log(arrayPoint);
-$("#paragraph1").html("Hence the function value at " + xIntercept + ',' + '0' + 'is'   + arrayPoint[1]);
-$("#paragraph1").animate({width:'toggle'},1000);
-$("#paragraph2").html("Hence the the function value at " + '0' + ',' + yIntercept + 'is'   + arrayPoint[2]);
-$("#paragraph2").animate({width:'toggle'},1000);
-$("#paragraph3").html("Hence the the function value at " + x + ',' + y + 'is'   + arrayPoint[3]);
-$("#paragraph3").animate({width:'toggle'},1000);
-setTimeout(function(){
+
+	$("#paragraphMain").html("Hence the maximum value of the function is "  + arrayPoint[3].toFixed(2));
 	arrayPoint.sort(function(a,b){ return (a-b)});
-console.log(arrayPoint)
-$("#paragraph").html("Hence the max is "  + arrayPoint[3]);
-$("#paragraph").animate({width:'toggle'},1000);
-},4000);
 
-
-
+	setTimeout(function(){
+		$("#paragraphMain").fadeToggle("slow", "linear");
+	},timeDuration + delay);
 }
